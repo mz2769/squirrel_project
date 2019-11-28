037d66f090921db9e85a31bb8d137b06862eeebe
Mengting Zheng <mz2769@columbia.com>
2019-11-28 23:08:50 +0000

    add app in setting, change allowed host

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#&wxp(!m6q^0fi5^tbj^ir#5(5#y2fmfn%9185h60$4$1j!bd&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'squirrel',
 ]
 
 MIDDLEWARE = [
