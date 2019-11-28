13a9bcae5c15fcd934bfbd2e718eac2455d012ff
Mengting Zheng <mz2769@columbia.com>
2019-11-28 23:05:47 +0000

    init and build model

---
+++ squirrel/models.py
@@ -0,0 +1,143 @@
+from django.db import models
+
+class Squirrel(models.Model):
+    latitude = models.DecimalField(max_digits = 13, decimal_places = 2)
+    longitude = models.DecimalField(max_digits = 13, decimal_places = 2)
+    unique_squirrel_id = models.CharField(
+        max_length=200,
+        unique=True,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return self.unique_squirrel_id
+
+    Morning = 'AM'
+    Afternoon = 'PM'
+    OCCUR_TIME_CHOICES = [
+        (Morning,'AM'),
+        (Afternoon,'PM'),
+    ]
+    shift = models.CharField(
+        max_length=2,
+        choices=OCCUR_TIME_CHOICES,
+        default=Morning,
+    )
+
+    date = models.DateField()
+
+    Adult = 'Adult'
+    Juvenile = 'Juvenile'
+    AGE_CHOICES = [
+        (Adult,'Adult'),
+        (Juvenile,'Juvenile'),
+    ]
+    age = models.CharField(
+        max_length=20,
+        choices=AGE_CHOICES,
+        default=None,
+    )
+    Gray = 'Gray'
+    Cinnamon = 'Cinnamon'
+    Black = 'Black'
+    COLOR_CHOICES = [
+        (Gray,'Gray'),
+        (Cinnamon,'Cinnamon'),
+        (Black,'Black'),
+    ]
+    primary_fur_color = models.CharField(
+        max_length=20,
+        choices=COLOR_CHOICES,
+        default=None,
+    )
+
+    A = 'Ground Plane'
+    B = 'Above Ground'
+    LOCATION_CHOICES = [
+        (A,'Ground Plane'),
+        (B,'Above Ground'),
+    ]
+    location = models.CharField(
+        max_length=40,
+        choices=LOCATION_CHOICES,
+        default=None,
+    )
+
+    specific_location = models.CharField(max_length=200,default=None)
+
+    T = 'TRUE'
+    F = 'FALSE'
+    SELECTION_CHOICE = [
+        (T,'TRUE'),
+        (F,'FALSE'),
+    ]
+    running = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+
+    chasing = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+
+    climbing = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+
+    eating = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    foraging = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    other_activities = models.CharField(max_length=200,default=None)
+
+    kuks = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    quaas = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    moans = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    tail_flags = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    tail_twitches = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    approaches = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    indifferent = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
+    runs_from = models.CharField(
+        max_length=20,
+        choices=SELECTION_CHOICE,
+        default=T,
+    )
