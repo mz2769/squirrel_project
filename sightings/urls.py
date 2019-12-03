6d867b85a501eaeeff02f3556158bd01c6e2f9cb
fanxu67 <56941093+fanxu67@users.noreply.github.com>
2019-12-02 20:06:32 -0500

    update urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('',views.list_sq,name='list_sq'),
     path('add/',views.create,name='create'),
     path('<unique_squirrel_id>/',views.update,name='update'),
+    path('<unique_squirrel_id>/',views.delete,name='delete''),
 
 # path('stats/',),
+
 ]
