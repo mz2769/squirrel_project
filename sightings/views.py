1a35675f7471f4c2212816afac98a6adaf913715
fanxu67 <56941093+fanxu67@users.noreply.github.com>
2019-12-02 20:01:35 -0500

    Update views.py

--- sightings/views.py
+++ sightings/views.py
@@ -41,3 +41,22 @@ def update(request,unique_squirrel_id):
     else:
         form = SquirrelForm(instance= obj)
         return render(request,'sightings/update.html' , {'form': form})
+
+def delete(request,unique_squirrel_id):
+        obj= get_object_or_404(Squirrel, pk=unique_squirrel_id)
+        if request.method == 'Delete':
+            form = SquirrelForm(request.Delete, instance= obj)
+            context= {'form': form}
+            if form.is_valid():
+                obj= form.save(commit= False)
+                obj.save()
+                messages.success(request, "You have successfully deleted the squirrel sighting")
+                context= {'form': form}
+                return render(request,'sightings/delete.html' , context)
+            else:
+                context= {'form': form,
+                          'error': 'The form was not deleted successfully. Please enter in valid info'}
+                return render(request,'sightings/delete.html' , context)
+        else:
+            form = SquirrelForm(instance= obj)
+            return render(request,'sightings/delete.html' , {'form': form})
