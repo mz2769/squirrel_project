bf010f2c8a21dcd83cc9b1f41fddf3bb12400893
fanxu67 <56941093+fanxu67@users.noreply.github.com>
2019-12-04 17:07:04 -0500

    remove delete

--- sightings/urls.py
+++ sightings/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('',views.list_sq,name='list_sq'),
     path('add/',views.create,name='create'),
     path('<unique_squirrel_id>/',views.update,name='update'),
-    path('<unique_squirrel_id>/',views.update,name='delete'),
 ]
 
 app_name = 'map'
--- sightings/views.py
+++ sightings/views.py
@@ -27,30 +27,21 @@ def create(request):
         form=SquirrelForm()
         return render(request, 'sightings/create.html', {'form': form})
 
-def update(request,unique_squirrel_id,del=False):
-    if del=False:
-        obj= get_object_or_404(Squirrel, pk=unique_squirrel_id)
-        if request.method == 'POST':
-            form = SquirrelForm(request.POST, instance= obj)
+def update(request,unique_squirrel_id):
+    obj= get_object_or_404(Squirrel, pk=unique_squirrel_id)
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST, instance= obj)
+        context= {'form': form}
+        if form.is_valid():
+            obj= form.save(commit= False)
+            obj.save()
+            messages.success(request, "You have successfully updated the squirrel sighting")
             context= {'form': form}
-            if form.is_valid():
-                obj= form.save(commit= False)
-                obj.save()
-                messages.success(request, "You have successfully updated the squirrel sighting")
-                context= {'form': form}
-                return render(request,'sightings/update.html' , context)
-            else:
-                context= {'form': form,
-                          'error': 'The form was not updated successfully. Please enter in valid info'}
-                return render(request,'sightings/update.html' , context)
+            return render(request,'sightings/update.html' , context)
         else:
-            form = SquirrelForm(instance= obj)
-            return render(request,'sightings/update.html' , {'form': form})
-
-
+            context= {'form': form,
+                      'error': 'The form was not updated successfully. Please enter in valid info'}
+            return render(request,'sightings/update.html' , context)
     else:
-        obj= get_object_or_404(Squirrel, pk=unique_squirrel_id)
-        # form = SquirrelForm(request.POST, instance= obj)
-        # context= {'form': form}
-        obj.delete()
-        messages.success(request, "You have successfully deleted the squirrel sighting")
+        form = SquirrelForm(instance= obj)
+        return render(request,'sightings/update.html' , {'form': form})
