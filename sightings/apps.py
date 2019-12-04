4a8e58941c1a17834ad7fe559ddda42d6d8458b5
fanxu67 <56941093+fanxu67@users.noreply.github.com>
2019-12-04 18:49:09 -0500

    urls

--- sightings/apps.py
+++ sightings/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class SquirrelConfig(AppConfig):
     name = 'sightings'
-
-class SquirrelConfig(AppConfig):
-    name = 'map'
--- sightings/urls.py
+++ sightings/urls.py
@@ -5,12 +5,8 @@ from . import views
 
 app_name = 'sightings'
 urlpatterns = [
+    path('map/',views.map,name='map'),
     path('',views.list_sq,name='list_sq'),
     path('add/',views.create,name='create'),
     path('<unique_squirrel_id>/',views.update,name='update'),
-]
-
-app_name = 'map'
-urlpatterns =[
-    path('',include('map.urls')),
 ]
