9abbad8c82a22705aa241d00e91382a207b07c1f
fanxu67 <56941093+fanxu67@users.noreply.github.com>
2019-12-04 19:12:31 -0500

    map correction

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
     path('map/',views.map,name='map'),
-    path('',views.list_sq,name='list_sq'),
-    path('add/',views.create,name='create'),
-    path('<unique_squirrel_id>/',views.update,name='update'),
+    path('sightings',views.list_sq,name='list_sq'),
+    path('sightings/add/',views.create,name='create'),
+    path('sightings/<unique_squirrel_id>/',views.update,name='update'),
 ]
